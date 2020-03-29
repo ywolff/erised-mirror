497511c01f334a8e014e3850c11c5fac9cedcd19
ywolff <ybwolff@gmail.com>
2020-03-29 17:16:06 +0200

    Make command work from outside package directory

--- start-with-ngrok.sh
+++ start-with-ngrok.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-trap 'exit' INT TERM ERR
-trap 'kill 0' EXIT
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
+cd "$(dirname "$0")/$(dirname "$(readlink "$0")")"
 
 yarn start > /dev/null &
 ngrok http -log=stdout 5000 > /dev/null &

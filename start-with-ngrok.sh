5864d0b54ca04921a9b533691b0ae7f977aa3862
ywolff <ybwolff@gmail.com>
2020-04-02 23:57:03 +0200

    Make user does not need to have yarn and ngrok installed

--- start-with-ngrok.sh
+++ start-with-ngrok.sh
@@ -4,8 +4,8 @@ trap "kill 0" EXIT
 
 cd "$(dirname "$0")/$(dirname "$(readlink "$0")")"
 
-yarn start > /dev/null &
-ngrok http -log=stdout 5000 > /dev/null &
+npm start > /dev/null &
+node_modules/.bin/ngrok http -log=stdout 5000 > /dev/null &
 
 echo "Building Mirror of Erised..."
 sleep 2
